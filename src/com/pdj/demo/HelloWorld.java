86308ed1555f67f54d75dc064983147388f880a5
park96323 <park96323@outlook.com>
2018-06-21 21:42:06 +0800

    Initial commit

---
+++ src/com/pdj/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.pdj.demo;
+
+/*
+ * Created by pdj on 2018-6-21
+ *  */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
